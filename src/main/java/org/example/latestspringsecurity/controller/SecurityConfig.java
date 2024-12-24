8bd63c53b44897e578f9bfe0438a33e1e3485f04
ocean <abc@qq.com>
2024-12-24 13:39:25 +0800

    httpBasic 是基于 basic 的模式弹出框进行认证的，认证成功之后，在 request headers 里面会有这么一段字符串 authorization: Basic YWJjOmFhYWFh

--- src/main/java/org/example/latestspringsecurity/controller/SecurityConfig.java
+++ src/main/java/org/example/latestspringsecurity/controller/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()
