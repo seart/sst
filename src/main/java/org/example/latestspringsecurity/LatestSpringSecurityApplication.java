ea2d0018e4a440d1eac0b0ed0c26c448abe2d0a5
ocean <abc@qq.com>
2024-12-24 11:51:11 +0800

    first commit user login use yml config file

---
+++ src/main/java/org/example/latestspringsecurity/LatestSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.latestspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatestSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/latestspringsecurity/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.example.latestspringsecurity.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+
+}
