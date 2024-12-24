22e0245c71d6b965655c2486459b49a9d57d13fd
ocean <abc@qq.com>
2024-12-24 17:31:29 +0800

    form表单登录，退出

--- src/main/java/org/example/latestspringsecurity/controller/SecurityConfig.java
+++ src/main/java/org/example/latestspringsecurity/controller/SecurityConfig.java
@@ -2,9 +2,9 @@ package org.example.latestspringsecurity.controller;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +14,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/js/**", "/css/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(form -> form.loginPage("/login.html").permitAll()
+                        //指登录成功后，是否始终跳转到登录成功url。它默认为false
+                        .defaultSuccessUrl("/index.html", true)
+                        //post登录接口，登录验证由系统实现
+                        .loginProcessingUrl("/login")
+                        .failureUrl("/error.html")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                )
+                // logout 暂时没用上
+//                .logout(logout -> logout.logoutUrl("/logout.html")
+//                        .permitAll()
+//                )
+                .csrf(AbstractHttpConfigurer::disable)
+        ;
         return http.build();
     }
 }
